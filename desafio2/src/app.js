2bd22bcc2ff7b0ef31cf9918fe20fd9ad9ffe616
marianorodriguez383 <marianorodriguez383@gmail.com>
2023-07-13 02:11:43 -0300

    correcciones desafio 3

--- desafio2/src/app.js
+++ desafio2/src/app.js
@@ -5,26 +5,34 @@ const app = express();
 const productManager = new ProductManager('products.json');
 const PORT = 8080;
 
-app.get('/products', (req, res) => {
-  const limit = req.query.limit;
-  const products = productManager.getProducts();
+app.get('/products', async (req, res) => {
+  try {
+    const products = productManager.getProducts();
+    const limit = req.query.limit;
 
-  if (limit) {
-    const limitedProducts = products.slice(0, limit);
-    res.json(limitedProducts);
-  } else {
-    res.json(products);
+    if (limit) {
+      const limitedProducts = products.slice(0, limit);
+      res.json(limitedProducts);
+    } else {
+      res.json(products);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener los productos' });
   }
 });
 
-app.get('/products/:pid', (req, res) => {
-  const pid = parseInt(req.params.pid);
-  const product = productManager.getProductById(pid);
+app.get('/products/:pid', async (req, res) => {
+  try {
+    const pid = parseInt(req.params.pid);
+    const product = productManager.getProductById(pid);
 
-  if (product) {
-    res.json(product);
-  } else {
-    res.status(404).json({ error: 'Producto no encontrado' });
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: 'Producto no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener el producto' });
   }
 });
 
